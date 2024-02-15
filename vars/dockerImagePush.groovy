c91ff0b8b01ee2d21609282289c9ad2d32fd3add
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-02-15 18:53:58 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,7 +1,11 @@
-def call(String aws_account_id, String region, String ecr_repoName){
+def call(String awsAccountId, String region, String ecrRepoName, String imageName, String tagName) {
+    def registry = "670855725719.dkr.ecr.ap-south-1.amazonaws.com/testecr"
     
-    sh """
-     aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 670855725719.dkr.ecr.ap-south-1.amazonaws.com
-     docker push 670855725719.dkr.ecr.ap-south-1.amazonaws.com/testecr
-    """
+    // Log in to AWS ECR
+    def loginCommand = "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${registry}"
+    sh(loginCommand)
+    
+    // Push the Docker image to AWS ECR
+    def pushCommand = "docker push ${registry}/${imageName}:${tagName}"
+    sh(pushCommand)
 }
