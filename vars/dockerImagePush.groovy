0772a34adba8a0c41749db90e268fb2620c35c8c
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-02-15 18:21:58 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -2,6 +2,6 @@ def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
      aws ecr get-login-password --region ap-south-1 | docker login --docker-ecr AWS --password-stdin $registry
-     docker push ${registry}
+     docker push ${registry}/${imagename}:${tagname}
     """
 }
